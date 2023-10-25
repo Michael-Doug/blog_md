bd2328340c61f5d98dd4c09321267434e34e137f
Michael Douglas <michael.emboava20@gmail.com>
2023-10-25 15:54:38 -0300

    Adicionado o session controller e configurando rotas para não dar erro na hora do login por procurar os parametros de session

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::SessionsController < ApplicationController
+    wrap_parameters format: []
+end
--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,8 @@ module BlogMd
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    
-    # alterei aqui - mudar aqui para reverter
+
+    # alterei aqui para validate_token funcionar - mudar aqui para reverter
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
